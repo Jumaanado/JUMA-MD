b39b7963633318ffd58adc25614f4f1d41d6cd3f
HANSTZ <hanstzmd@gmail.com>
2024-11-18 11:14:55 +0300

    Update index.js

--- framework/index.js
+++ framework/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.genererNomFichier = exports.stick = exports.format = exports.styletext = exports.zJson = exports.getBuffer = exports.reaction = exports.police = exports.zokou = void 0;
-let { zokou } = require("./zokou");
+let { zokou } = require("./zokau");
 exports.zokou = zokou;
 const mesfonctions_1 = require("./mesfonctions");
 Object.defineProperty(exports, "reaction", { enumerable: true, get: function () { return mesfonctions_1.reaction; } });
@@ -12,5 +12,5 @@ Object.defineProperty(exports, "format", { enumerable: true, get: function () { 
 Object.defineProperty(exports, "styletext", { enumerable: true, get: function () { return mesfonctions_1.styletext; } });
 Object.defineProperty(exports, "stick", { enumerable: true, get: function () { return mesfonctions_1.stick; } });
 Object.defineProperty(exports, "genererNomFichier", { enumerable: true, get: function () { return mesfonctions_1.genererNomFichier; } });
-var { reagir } = require("./app");
+var { reagir } = require("./hans");
 
